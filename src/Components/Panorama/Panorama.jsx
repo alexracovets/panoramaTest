4e975b4ae94168004e8711804aadcad2020df4db
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2023-09-14 16:27:05 +0300

    fixed vr

--- src/Components/Panorama/Panorama.jsx
+++ src/Components/Panorama/Panorama.jsx
@@ -36,7 +36,7 @@ export default function MyScene() {
     return (
         <>
             <Scene cursor="rayOrigin: mouse" raycaster="objects: .cursor-listener" class="panorama" vr-mode-ui="enabled: false" exit-vr>
-                <a-camera orbit-controls="target: 0 1.6 -0.5; minDistance: 0.5; maxDistance: 180; initialPosition: 0 5 15">
+                <a-camera orbit-controls="target: 0 1.6 -0.5; minDistance: 0.5; maxDistance: 180; initialPosition: 0 5 15" look-controls="magicWindowTrackingEnabled: false">
                     <a-sky src={texture} rotation="0 -130 0" crossorigin="anonymous"></a-sky>
                     {
                         panorama.panoramaInteractive.mask.length > 0 && (
